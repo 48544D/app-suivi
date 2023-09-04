16e463d53d0f942553dccd1f0cd05c6c7e2d4171
HTM <haythamaaraba7@gmail.com>
2023-09-04 14:00:14 +0100

    Licensing

---
+++ README.md
@@ -0,0 +1,1 @@
+App de suivi des dépôts © 2021 by Haytham aaraba is licensed under CC BY-NC-SA 4.0. To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-sa/4.0/
--- index.php
+++ index.php
@@ -1,27 +1,27 @@
 <?php
-    include './includes/connection.php';
-    @session_start();
-    //check if user is logged in
-    if (isset($_SESSION['user']) && isset($_SESSION['Admin'])) {
-        header('Location: ./admin/admin.php');
-    }elseif (isset($_SESSION['user'])) {
-        header('Location: ./user/user.php');
-    }
+include './includes/connection.php';
+@session_start();
+//check if user is logged in
+if (isset($_SESSION['user']) && isset($_SESSION['Admin'])) {
+    header('Location: ./admin/admin.php');
+} elseif (isset($_SESSION['user'])) {
+    header('Location: ./user/user.php');
+}
 
-    if (isset($_POST['login'])) {
-        $username = $_POST['username'];
-        $password = $_POST['password'];
-        $user = $db->query('SELECT * FROM users WHERE username = "' . $username . '" AND password = "' . $password . '"')->fetch();
-        if ($user) {
-            if  ($user['isAdmin'] == 1)
-                $_SESSION['Admin'] = TRUE;
-            $_SESSION['user'] = $username;
-            $_SESSION['id'] = $user['id'];
-            header('Location: index.php');
-        } else {
-            $error = '<div class="error"><h3>Veuillez vérifier votre nom d\'utilisateur et votre mot de passe</h3></div>';
-        }
+if (isset($_POST['login'])) {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $user = $db->query('SELECT * FROM users WHERE username = "' . $username . '" AND password = "' . $password . '"')->fetch();
+    if ($user) {
+        if ($user['isAdmin'] == 1)
+            $_SESSION['Admin'] = TRUE;
+        $_SESSION['user'] = $username;
+        $_SESSION['id'] = $user['id'];
+        header('Location: index.php');
+    } else {
+        $error = '<div class="error"><h3>Veuillez vérifier votre nom d\'utilisateur et votre mot de passe</h3></div>';
     }
+}
 ?>
 
 <!DOCTYPE html>
@@ -61,6 +61,7 @@
             </div>
         </div>
     </div>
+    <p xmlns:cc="http://creativecommons.org/ns#" xmlns:dct="http://purl.org/dc/terms/"><a property="dct:title" rel="cc:attributionURL" href="https://github.com/48544D/app-suivi">App de suivi des dépôts</a> by <a rel="cc:attributionURL dct:creator" property="cc:attributionName" href="https://github.com/48544D">Haytham aaraba</a> is licensed under <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/?ref=chooser-v1" target="_blank" rel="license noopener noreferrer" style="display:inline-block;">CC BY-NC-SA 4.0<img style="height:22px!important;margin-left:3px;vertical-align:text-bottom;" src="https://mirrors.creativecommons.org/presskit/icons/cc.svg?ref=chooser-v1"><img style="height:22px!important;margin-left:3px;vertical-align:text-bottom;" src="https://mirrors.creativecommons.org/presskit/icons/by.svg?ref=chooser-v1"><img style="height:22px!important;margin-left:3px;vertical-align:text-bottom;" src="https://mirrors.creativecommons.org/presskit/icons/nc.svg?ref=chooser-v1"><img style="height:22px!important;margin-left:3px;vertical-align:text-bottom;" src="https://mirrors.creativecommons.org/presskit/icons/sa.svg?ref=chooser-v1"></a></p>
 </body>
 
 </html>
